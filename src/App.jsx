3c6ed7fc8f8a37617f648f47a8f71715ddc1b78a
Anthony Chen <54040348+Anzchen@users.noreply.github.com>
2024-08-07 21:06:18 -0400

    Tailwind
    npm install -D tailwindcss postcss autoprefixer
    npx tailwindcss init -p

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
-const App = () => {
-  return <h1>Hello</h1>
+export default function App() {
+  return (
+    // <h1 className="text-3xl font-bold underline">
+    //   Hello world!
+    // </h1>
+
+    <div className="h-screen flex bg-gray-950 justify center place-items-center">
+      <div className="text-9xl font-bold text-gray-100">Welcome.</div>
+    </div>
+  )
 }
