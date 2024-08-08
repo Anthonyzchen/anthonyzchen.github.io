5bf725bc04df745991e074a4cfe1971184b71945
Anthony Chen <54040348+Anzchen@users.noreply.github.com>
2024-08-07 21:57:11 -0400

    Setup for GSAP implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
+
 export default function App() {
+  // Creates a reference to the root level element, for scoping
+  const comp = useRef(null);
+
+  // Fires after all the DOM mutations have been done.
+  // The function passed into useLayoutEffect will run once after the component mounts and once again after it unmounts due to the empty dependency array.
+  useLayoutEffect(() => {
+    // Record all gsap animations that are setup during the context execution for easy cleanup.
+    // comp is used here for scoping, so that all the animations we create only affect children of the comp.
+    let ctx = gsap.context(() => {
+      // Gsap timeline helps sequence complex animations without dealing with animation timings.
+      const tl = gsap.timeline();
+    }, comp);
+
+    // When the effect function is about to be unmounted or cleaned up, revert all animations. (to prevent memory leaks, etc.)
+    return () => ctx.revert();
+  }, []);
+
   return (
     <div className="relative">
-      <div className="absolute left-0 top-0 z-10 flex h-screen w-full flex-col gap-10 bg-gray-50 p-10 font-KoHo tracking-tight">
+      <div id="preloader" className="absolute left-0 top-0 z-10 flex h-screen w-full flex-col gap-10 bg-gray-50 p-10 font-KoHo tracking-tight">
+        <h1 className="text-9xl" id="">Software Engineer</h1>
+        <h1 className="text-9xl">Welcome to my page</h1>
         <h1 className="text-9xl">Software Engineer</h1>
-        <h1 className="scale-125 opacity-50 hover:scale-150 hover:opacity-75">
-          Software Engineer
-        </h1>
-        <h1>Software Engineer</h1>
       </div>
       <div className="flex h-screen place-items-center justify-center bg-gray-950">
         <div className="font-KoHo text-9xl font-bold text-gray-100">
