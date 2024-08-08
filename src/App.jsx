b0f352fe89a81ec5242afbced734723040ca4942
Anthony Chen <54040348+Anzchen@users.noreply.github.com>
2024-08-07 21:27:41 -0400

    Tailwind + Prettier
    npm install -D prettier prettier-plugin-tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 export default function App() {
   return (
-    // <h1 className="text-3xl font-bold underline">
-    //   Hello world!
-    // </h1>
-
-    <div className="h-screen flex bg-gray-950 justify center place-items-center">
-      <div className="text-9xl font-bold text-gray-100">Welcome.</div>
+    <div className="relative">
+      <div className="w-full h-screen p-10 bg-gray-50 absolute top-0 left-0 font-KoHo z-10 flex flex-col gap-10 tracking-tight">
+        <h1 className="text-9xl">Software Engineer</h1>
+        <h1>Software Engineer</h1>
+        <h1>Software Engineer</h1>
+      </div>
+      <div className="h-screen flex bg-gray-950 justify-center place-items-center">
+        <div className="text-9xl text-gray-100 font-bold font-KoHo">
+          Welcome.
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        KoHo: ["KoHo", "sans-serif"],
+      }
+    },
   },
-  plugins: [],
+  plugins: ["prettier-plugin-tailwindcss"],
 }
 
